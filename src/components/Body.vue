52c98be44fd4be65c03f22bc3e9212968d3c1a84
Lidia Peleja <lidiapeleja@gmail.com>
2020-05-09 17:53:23 +0200

    added conditional rendering depending on form validation

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -5,35 +5,38 @@
         class="col d-flex align-items-center justify-content-center text-center"
       >
         <div>
+           <p v-if="submitFormisInvalid" class="mb-4 text-danger warning-message">
+               <span>⚠️</span> Submit the color in HEX value. Example: #ffffff
+              </p>
           <div>
             <h1>
               mix(
               <b-input
-                v-bind:value="color1"
-                id="color1"
+                v-bind:value="form.color1"
+                id="form.color1"
                 class="h4 mb-2 mr-sm-2 mb-sm-0"
-                placeholder="$color-1"
-              ></b-input
-              >,
+                placeholder="#cc5490"
+              ></b-input>
+              ,
               <span>
                 <b-input
-                  v-bind:value="color2"
-                  id="color2"
+                  v-bind:value="form.color2"
+                  id="form.color2"
                   class="h4 mb-2 mr-sm-2 mb-sm-0"
-                  placeholder="$color-2"
+                  placeholder="#fff"
                 ></b-input></span
               >,
               <span>
                 <b-input
-                  v-bind:value="weight"
-                  id="weight"
+                  v-bind:value="form.weight"
+                  id="form.weight"
                   class="h4 mb-2 mr-sm-2 mb-sm-0"
-                  placeholder="$weight"
+                  placeholder="50%"
                 ></b-input></span
               >)
             </h1>
             <b-button v-on:click="submitForm" class="section" variant="primary"
-              >Find the color</b-button
+              >Find color</b-button
             >
           </div>
         </div>
@@ -45,19 +48,31 @@
 <script>
 export default {
   name: 'Body',
-  methods: {
-    submitForm() {
-      console.log('is here anything working?');
-    },
-  },
   components: {},
   data() {
     return {
-      color1: '',
-      color2: '',
-      weight: '',
-      bgColor: 'yellow',
+      form: {
+        submitFormisInvalid: false,
+        color1: null,
+        color2: null,
+        weight: null,
+        bgColor: null,
+      },
     };
+  },
+  methods: {
+    submitForm() {
+      const submitFormisInvalid =
+        this.form.color1 === null &&
+        this.form.color2 === null &&
+        this.form.weight === null;
+
+      if (submitFormisInvalid) {
+        console.log('errrrror');
+      } else {
+        console.log('working');
+      }
+    },
   },
 };
 </script>
