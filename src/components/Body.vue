0421370d57871872fdd2749fa5f6abeb074ee508
Lidia Peleja <lidiapeleja@gmail.com>
2020-05-09 18:27:35 +0200

    fixded logics for form validator

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -5,37 +5,38 @@
         class="col d-flex align-items-center justify-content-center text-center"
       >
         <div>
-           <p v-if="submitFormisInvalid" class="mb-4 text-danger warning-message">
-               <span>⚠️</span> Submit the color in HEX value. Example: #ffffff
-              </p>
+          <p
+            v-if="!submitFormisInvalid"
+            class="mb-4 text-danger warning-message"
+          >
+            <span>⚠️</span> Submit the color in HEX value. Example: #ffffff
+          </p>
           <div>
             <h1>
               mix(
               <b-input
-                v-bind:value="form.color1"
-                id="form.color1"
+                v-model="form.color1"
                 class="h4 mb-2 mr-sm-2 mb-sm-0"
                 placeholder="#cc5490"
               ></b-input>
               ,
               <span>
                 <b-input
-                  v-bind:value="form.color2"
-                  id="form.color2"
+                  v-model="form.color2"
                   class="h4 mb-2 mr-sm-2 mb-sm-0"
                   placeholder="#fff"
                 ></b-input></span
               >,
               <span>
                 <b-input
-                  v-bind:value="form.weight"
-                  id="form.weight"
+                  v-model="form.weight"
                   class="h4 mb-2 mr-sm-2 mb-sm-0"
                   placeholder="50%"
                 ></b-input></span
               >)
             </h1>
-            <b-button v-on:click="submitForm" class="section" variant="primary"
+            {{ form.color1 }}
+            <b-button @click="submitForm" class="section" variant="primary"
               >Find color</b-button
             >
           </div>
@@ -53,9 +54,9 @@ export default {
     return {
       form: {
         submitFormisInvalid: false,
-        color1: null,
-        color2: null,
-        weight: null,
+        color1: '',
+        color2: '',
+        weight: '',
         bgColor: null,
       },
     };
@@ -63,14 +64,14 @@ export default {
   methods: {
     submitForm() {
       const submitFormisInvalid =
-        this.form.color1 === null &&
-        this.form.color2 === null &&
-        this.form.weight === null;
+        this.form.color1 === '' ||
+        this.form.color2 === '' ||
+        this.form.weight === '';
 
       if (submitFormisInvalid) {
-        console.log('errrrror');
+        alert('errrrror');
       } else {
-        console.log('working');
+        alert('working');
       }
     },
   },
