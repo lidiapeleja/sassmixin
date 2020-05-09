ca041330e928b67d13bed1e3d589cb5dc9cfabc0
Lidia Peleja <lidiapeleja@gmail.com>
2020-05-09 15:24:37 +0200

    added v-binds + first stat structre to component

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -8,37 +8,33 @@
           <div>
             <h1>
               mix(
-              <label class="sr-only" for="inline-form-input-name">Name</label>
               <b-input
-                id="inline-form-input-name"
+                v-bind:value="color1"
+                id="color1"
                 class="mb-2 mr-sm-2 mb-sm-0"
                 placeholder="$color-1"
               ></b-input
               >,
               <span>
-                <label class="sr-only" for="inline-form-input-name">Name</label>
                 <b-input
-                  id="inline-form-input-name"
+                  v-bind:value="color2"
+                  id="color2"
                   class="mb-2 mr-sm-2 mb-sm-0"
                   placeholder="$color-2"
                 ></b-input></span
               >,
               <span>
-                <label class="sr-only" for="inline-form-input-name">Name</label>
                 <b-input
-                  id="inline-form-input-name"
+                  v-bind:value="weight"
+                  id="weight"
                   class="mb-2 mr-sm-2 mb-sm-0"
                   placeholder="$weight"
                 ></b-input></span
               >)
             </h1>
-            <b-button class="section" variant="primary">Find the color</b-button>
-          </div>
-          <div class="article">
-            <p>
-              for example: mix(<span>#fff</span>, <span>#5678fd</span>,
-              <span>50%</span>)
-            </p>
+            <b-button class="section" variant="primary"
+              >Find the color</b-button
+            >
           </div>
         </div>
       </div>
@@ -50,6 +46,14 @@
 export default {
   name: 'Body',
   components: {},
+  data() {
+    return {
+      color1: '',
+      color2: '',
+      weight: '',
+      bgColor: 'yellow',
+    };
+  },
 };
 </script>
 
